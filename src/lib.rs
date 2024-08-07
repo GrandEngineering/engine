63693ffbc413d6f5b53528f4095995289a4ccc07
IGN-Styly <claudioresendeptbr@gmail.com>
2024-08-07 16:08:54 +0100

    update lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub enum TaskRunner {
+    GPU,
+    CPU,
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub enum Task {
+    AksPrime,
 }
