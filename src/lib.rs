21e0c07efcb12ca925155861406cc3da74845064
IGN-Styly <claudioresendeptbr@gmail.com>
2024-08-08 00:44:15 +0100

    boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
 pub enum TaskRunner {
-    GPU,
     CPU,
+    ROCM,
+    CUDA,
 }
-pub enum Task {
-    AksPrime,
+pub struct Chunk {
+    pub start: u128,
+    pub end: u128,
+}
+
+pub struct Task<TaskInput, TaskOutput> {
+    pub task_runner: TaskRunner,
+    pub task_id: u128,
+    pub task_fn: Box<dyn Fn(TaskInput, &TaskRunner) -> TaskOutput>,
+}
+impl<TaskInput, TaskOutput> Task<TaskInput, TaskOutput> {
+    pub fn execute(&self, input: TaskInput) -> TaskOutput {
+        (self.task_fn)(input, &self.task_runner)
+    }
+}
+
+pub fn isprime(input: Chunk, runner: &TaskRunner) -> u128 {
+    input.end * (input.start + input.end)
 }
