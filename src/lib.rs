3305302daa56c4c1df3417c51828abc17bd92fe4
IGN-Styly <claudioresendeptbr@gmail.com>
2024-09-12 15:59:02 +0100

    remove test file

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub trait Task {
     fn run_hip(&mut self) {
-        println!("HIP Runtime not availible running with the CPU");
+        println!("HIP Runtime not available, falling back to CPU");
         self.run_cpu();
     }
     fn run_cpu(&mut self) {
@@ -9,11 +9,13 @@ pub trait Task {
     fn run(&mut self, run: Option<Runner>) {
         match run {
             Some(Runner::HIP) => self.run_hip(),
-            Some(Runner::CPU) => self.run_cpu(),
-            None => self.run_cpu(),
+            Some(Runner::CPU) | None => self.run_cpu(),
         }
     }
+    fn from_bytes(&[u8]) -> Self;
+    fn to_bytes(&self) -> &[u8];
 }
+#[derive(Debug, Clone, Copy)]
 pub enum Runner {
     HIP,
     CPU,
