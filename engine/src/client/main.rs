d61292eb083fd1b7eadbad18267e60183e9a0701
IGN-Styly <claudioresendeptbr@gmail.com>
2024-09-12 17:19:46 +0100

    client boilerplate

--- engine/src/client/main.rs
+++ engine/src/client/main.rs
@@ -1,12 +1,18 @@
+use enginelib;
 use proto::engine_client::EngineClient;
 use std::error::Error;
-use tonic;
 pub mod proto {
     tonic::include_proto!("engine");
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let url = "http://[::1]:50051"
+    let url = "http://[::1]:50051";
+    let mut client = EngineClient::connect(url).await?;
+    let req = proto::TaskRequest {
+        task_type: proto::TaskType::TaskFib.into(),
+    };
+    let res = client.aquire_task(req).await?;'
+    println!("{:?}", res);
     Ok(())
 }
