f826d52888e4764f9785723bb192b589a55ba9ca
IGN-Styly <claudioresendeptbr@gmail.com>
2024-09-12 16:13:19 +0100

    fixed sub obtiomal code

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,8 @@ pub trait Task {
             Some(Runner::CPU) | None => self.run_cpu(),
         }
     }
-    fn from_bytes(&[u8]) -> Self;
-    fn to_bytes(&self) -> &[u8];
+    fn from_bytes(bytes: &[u8]) -> Self;
+    fn to_bytes(&self) -> Vec<u8>;
 }
 #[derive(Debug, Clone, Copy)]
 pub enum Runner {
@@ -24,14 +24,14 @@ pub struct FibTask {
     iter: u64,
     result: u64,
 }
-impl Task for FibTask{
+impl Task for FibTask {
     fn run_cpu(&mut self) {
         let mut a = 0;
         let mut b = 1;
         for _ in 0..self.iter {
             let tmp = a;
             a = b;
-            b = tmp + b;
+            b += tmp;
         }
         self.result = a;
     }
@@ -40,7 +40,7 @@ impl Task for FibTask{
         let result = u64::from_le_bytes(bytes[8..16].try_into().unwrap());
         Self { iter, result }
     }
-    fn to_bytes(&self) -> &[u8] {
+    fn to_bytes(&self) -> Vec<u8> {
         let mut bytes = Vec::with_capacity(16);
         bytes.extend_from_slice(&self.iter.to_le_bytes());
         bytes.extend_from_slice(&self.result.to_le_bytes());
