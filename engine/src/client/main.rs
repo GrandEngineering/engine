6636722201ecbc76f2eecdaff88618c6b8f90561
IGN-Styly <claudioresendeptbr@gmail.com>
2024-09-12 17:16:23 +0100

    boilerplate

--- engine/src/client/main.rs
+++ engine/src/client/main.rs
@@ -1,6 +1,12 @@
+use proto::engine_client::EngineClient;
 use std::error::Error;
+use tonic;
+pub mod proto {
+    tonic::include_proto!("engine");
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "http://[::1]:50051"
     Ok(())
 }
