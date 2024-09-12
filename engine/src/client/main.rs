e543ac4af48178e540ed3fc3427e87a8a41dbe7a
IGN-Styly <claudioresendeptbr@gmail.com>
2024-09-12 17:26:35 +0100

    it works

--- engine/src/client/main.rs
+++ engine/src/client/main.rs
@@ -1,4 +1,4 @@
-use enginelib;
+use enginelib::*;
 use proto::engine_client::EngineClient;
 use std::error::Error;
 pub mod proto {
@@ -12,7 +12,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let req = proto::TaskRequest {
         task_type: proto::TaskType::TaskFib.into(),
     };
-    let res = client.aquire_task(req).await?;
-    println!("{:?}", res);
+    let request = tonic::Request::new(req);
+    let res = client.aquire_task(request).await?;
+    let data = res.get_ref();
+    let mut decoded: FibTask = enginelib::FibTask::from_bytes(data.task_payload.as_slice());
+    decoded.run(Some(Runner::CPU));
+    println!("{:?}", decoded);
     Ok(())
 }
