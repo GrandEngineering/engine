3635ddd81cf67f9048125ddceb8fbe8d08a9d8e1
IGN-Styly <claudioresendeptbr@gmail.com>
2024-11-05 20:20:10 +0000

    fix name

--- engine/src/bin/server.rs
+++ engine/src/bin/server.rs
@@ -14,8 +14,9 @@ mod proto {
     pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
         tonic::include_file_descriptor_set!("engine_descriptor");
 }
+
 #[derive(Default)]
-struct engine_api {
+struct engine_service {
     EngineAPI: EngineAPI,
 }
 
