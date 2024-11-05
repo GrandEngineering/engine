a986ceea08a297245cb97a1b4d5a7f495652e0a4
IGN-Styly <claudioresendeptbr@gmail.com>
2024-11-05 20:28:31 +0000

    retire deprecated method in tonic

--- engine/build.rs
+++ engine/build.rs
@@ -4,7 +4,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("engine_descriptor.bin"))
-        .compile(&["proto/engine.proto"], &["proto"])
+        .compile_protos(&["proto/engine.proto"], &["proto"])
         .unwrap();
     tonic_build::compile_protos("proto/engine.proto")?;
     Ok(())
